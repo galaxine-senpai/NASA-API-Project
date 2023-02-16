63aab40ef2d14c195407a1e400fc822e665f2299
galaxine-senpai <insta.is.good1234@gmail.com>
2023-02-15 17:42:06 -0800

    Updated code from yapudjus

--- marsroverapi.py
+++ marsroverapi.py
@@ -19,15 +19,14 @@ parser = argparse.ArgumentParser(
 # Arrays of rovers and cameras
 rovers = ["curiosity", "opportunity", "spirit"] # Rovers available
 cameras = ["NAVCAM", "FHAZ", "RHAZ", "MAST", "CHEMCAM", "MAHLI", "MARDI", "NAVCAM", "PANCAM", "MINITES"] # Cameras available
-def get_photos(rover, sol, camera):
+def get_photos(rover: str, sol: int, camera="NAVCAM"):
     """Get photos from the Nasa Mars Rover API"""
     # A ton of innecessary pre checks to make sure the user has provided the correct information
     if rover == "":
         print("No rover provided, exiting... please provide a rover")
         return
     elif rover.lower() not in rovers:
-        print("Rover not recognised, exiting... please provide a valid rover")
-        print(f"valid rovers are {rovers}")
+        print(f"Rover not recognised, exiting... please provide a valid rover, valid rovers are {', '.join(rovers)}")
         return
     if sol == "":
         print("No sol provided, exiting... please provide a sol")
@@ -35,11 +34,9 @@ def get_photos(rover, sol, camera):
     elif sol.isdigit() == False:
         print("Sol is not a number, exiting... please provide a number")
         return
-    if camera == "":
-        camera = "NAVCAM"
     elif camera.upper() not in cameras:
         print("Camera not recognised, exiting... please provide a valid camera")
-        print(f"valid cameras are {cameras}")
+        print(f"valid cameras are {', '.join(cameras)}")
         return
 
     url = f"{api}/rovers/{rover}/photos?sol={sol}&camera={camera}&api_key={apikey}"
@@ -52,14 +49,13 @@ def get_photos(rover, sol, camera):
     id = response.json()["photos"][0]["id"]
     return f"Photo URL: {photourl}\nEarth Date: {earthdate}\nPhoto ID: {id}"
 
-def get_status(rover):
+def get_status(rover: str):
     """Get status of a specific rover"""
     if rover == "":
         print("No rover provided, exiting... please provide a rover")
         return
     elif rover.lower() not in rovers:
-        print("Rover not recognised, exiting... please provide a valid rover")
-        print(f"valid rovers are {rovers}")
+        print(f"Rover not recognised, exiting... please provide a valid rover, valid rovers are {', '.join(rovers)}")
         return
 
     url = f"{api}/manifests/{rover}?api_key={apikey}"
@@ -70,7 +66,7 @@ def get_status(rover):
     launchdate = response.json()["photo_manifest"]["launch_date"]
     landingdate = response.json()["photo_manifest"]["landing_date"]
     status = response.json()["photo_manifest"]["status"]
-    return f"Launched: {launchdate}\nLanded on Mars: {landingdate},\n Is {rover} active?: {status}"
+    return f"Launched: {launchdate}\nLanded on Mars: {landingdate},\n Is {rover} active?: {status}" 
 
 #print(get_photos("curiosity", "100", "NAVCAM")) # Get photos from curiosity on sol 100 (testing)
 #print(get_status("curiosity")) # Get status of curiosity (testing)
