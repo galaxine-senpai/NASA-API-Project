1455f5f7cd39cbf24cf8bfc477a1d4716794eced
galaxine-senpai <insta.is.good1234@gmail.com>
2023-02-16 10:34:15 -0800

    Fixes

--- apodapi.py
+++ apodapi.py
@@ -23,8 +23,8 @@ def get_todays_photo():
     else:
         return f"Copyright/Author: {Copyright}\nTitle: {Title}\nDate Taken:{Date}\nPhoto URL: {Photo}" #TODO: Figure out why python complains
 
-def get_specific_apod(month: int, day: int, year: int):
-    """Get the APOD"""
+def get_specific_apod(month: int, day: int, year: int): #Month can be two or one didget (eg: 1 or 01, YEAR HAS TO BE 4)
+    """Get the APOD (date example: 4,26,2007"""
     r = requests.get(f"{api}?api_key={apikey}&date={year}-{month}-{day}")
     #Copyright = r.json()["copyright"] # Removed due to me being lazy and unable to figure out how to check if nothing is there
     Title = r.json()["title"]
@@ -37,7 +37,7 @@ def get_specific_apod(month: int, day: int, year: int):
         return f"Copyright/Author: N/A\nTitle: {Title}\nDate Taken:{Date}\nPhoto URL: {Photo}"
 
 #print(get_todays_photo()) # Tested: Good, no issues
-#print(get_specific_apod(4, 26, 2007)) # Tested: Good, no issues
+print(get_specific_apod(4, 26, 2007)) # Tested: Good, no issues
 
 """
 Made by galaxine~senapi
